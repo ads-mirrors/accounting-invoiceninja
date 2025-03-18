db20d7edea78c5c94b724bf7ecd464ef8adbd543
David Bomba <turbo124@gmail.com>
2025-03-19 10:03:30 +1100

    Fixes for markdown parsing

--- app/Services/Pdf/PdfBuilder.php
+++ app/Services/Pdf/PdfBuilder.php
@@ -59,6 +59,12 @@ class PdfBuilder
 
         $this->commonmark = new CommonMarkConverter([
             'allow_unsafe_links' => false,
+            'renderer' => [
+                'block_separator' => "\n",
+                'inner_separator' => "\n",
+                'soft_break'      => "\n",
+            ],
+            'html_input' => 'escape',
         ]);
     }
 
@@ -2096,21 +2102,17 @@ class PdfBuilder
 
             if ($this->service->company->markdown_enabled && $this->isMarkdown($child['content'])) {
 
-                $child['content'] = str_ireplace(['<br>', '<br/>', '<br />'], "\r", $child['content']); //13-05-2025 - testing whether /r or <br/> can work
-
-                // $child['content'] = str_ireplace(['<br>', '<br/>', '<br />'], "<br/>", $child['content']); //13-05-2025 - testing whether /r or <br/> can work
+                $child['content'] = str_ireplace(['<br>', '<br/>', '<br />'], "\r\n", $child['content']); //19-05-2025 - pivotting back to this, and allowing html_input for commonmark.
                 $child['content'] = $this->commonmark->convert($child['content']); //@phpstan-ignore-line
+                $child['content'] = nl2br($child['content']); //19-05-2025 - This allows us to use <br> and markdown and still get our spacing.
+                
             }
 
             $contains_html = str_contains($child['content'], '<') && str_contains($child['content'], '>');
 
-            nlog((string)$child['content']);
-
             if ($contains_html) {
                 // Encode any HTML elements now so that DOMDocument doesn't throw any errors,
                 // Later we can decode specific elements.
-
-                $child['content'] = str_replace("\n", "<br/>", $child['content']);
 
                 $_child = $this->document->createElement($child['element'], '');
                 $_child->setAttribute('data-state', 'encoded-html');
