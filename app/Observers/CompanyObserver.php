8596c57e5c817dd7ad7286f83e2986a176386a3f
David Bomba <turbo124@gmail.com>
2025-04-16 13:19:36 +1000

    Check changes to fields

--- app/Observers/CompanyObserver.php
+++ app/Observers/CompanyObserver.php
@@ -40,6 +40,22 @@ class CompanyObserver
             \Modules\Admin\Jobs\Domain\CustomDomain::dispatch($company->getOriginal('portal_domain'), $company)->onQueue('domain');
         }
 
+        if(Ninja::isHosted()){
+            
+            $property = 'name';
+            $original = data_get($this->getOriginal('settings'), $property);
+            $current = data_get($this->settings, $property);
+
+            if($original !== $current){
+                try {
+                    (new \Modules\Admin\Jobs\Account\FieldQuality())->checkCompanyName($current, $company);
+                } catch (\Throwable $e) {
+                    nlog(['company_name_check', $e->getMessage()]);
+                }
+            }
+
+        }
+
     }
 
     /**
