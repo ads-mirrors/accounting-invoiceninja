e7a63237f2ddbf8538ee403a7a2dac2f641c08d9
Daniel Barduhn <git@danielbarduhn.com>
2025-05-01 12:06:44 +0200

    added missing classmap entries

--- app/Services/EDocument/Standards/Verifactu/VerifactuClient.php
+++ app/Services/EDocument/Standards/Verifactu/VerifactuClient.php
@@ -13,6 +13,28 @@ use App\Services\EDocument\Standards\Verifactu\Types\RegistroFacturacionAlta;
 use App\Services\EDocument\Standards\Verifactu\Types\RegistroFacturacionAnulacion;
 use App\Services\EDocument\Standards\Verifactu\Types\RegistroFacturacionSubsanacion;
 use App\Services\EDocument\Standards\Verifactu\Types\Subsanacion;
+use App\Services\EDocument\Standards\Verifactu\Types\ImporteSgn14_2;
+use App\Services\EDocument\Standards\Verifactu\Types\Incidencia;
+use App\Services\EDocument\Standards\Verifactu\Types\ObligadoEmision;
+use App\Services\EDocument\Standards\Verifactu\Types\OperacionExenta;
+use App\Services\EDocument\Standards\Verifactu\Types\PersonaFisicaJuridica;
+use App\Services\EDocument\Standards\Verifactu\Types\PersonaFisicaJuridicaES;
+use App\Services\EDocument\Standards\Verifactu\Types\RechazoPrevio;
+use App\Services\EDocument\Standards\Verifactu\Types\RegistroAlta;
+use App\Services\EDocument\Standards\Verifactu\Types\RegistroAnterior;
+use App\Services\EDocument\Standards\Verifactu\Types\SistemaInformatico;
+use App\Services\EDocument\Standards\Verifactu\Types\Cabecera;
+use App\Services\EDocument\Standards\Verifactu\Types\Desglose;
+use App\Services\EDocument\Standards\Verifactu\Types\DesgloseRectificacion;
+use App\Services\EDocument\Standards\Verifactu\Types\Destinatarios;
+use App\Services\EDocument\Standards\Verifactu\Types\Detalle;
+use App\Services\EDocument\Standards\Verifactu\Types\DetalleDesglose;
+use App\Services\EDocument\Standards\Verifactu\Types\Encadenamiento;
+use App\Services\EDocument\Standards\Verifactu\Types\IDDestinatario;
+use App\Services\EDocument\Standards\Verifactu\Types\IDFactura;
+use App\Services\EDocument\Standards\Verifactu\Types\IDFacturaAR;
+use App\Services\EDocument\Standards\Verifactu\Types\IDFacturaExpedida;
+use App\Services\EDocument\Standards\Verifactu\Types\IDOtro;
 
 
 class VerifactuClient
@@ -56,12 +78,33 @@ class VerifactuClient
             'location'     => $endpoint,
             'soap_version' => SOAP_1_1,
             'classmap'     => [
+                'Cabecera'                       => Cabecera::class,
+                'Desglose'                       => Desglose::class,
+                'DesgloseRectificacion'          => DesgloseRectificacion::class,
+                'Destinatarios'                  => Destinatarios::class,
+                'Detalle'                        => Detalle::class,
+                'DetalleDesglose'                => DetalleDesglose::class,
+                'Encadenamiento'                 => Encadenamiento::class,
+                'IDDestinatario'                 => IDDestinatario::class,
+                'IDFactura'                      => IDFactura::class,
+                'IDFacturaAR'                    => IDFacturaAR::class,
+                'IDFacturaExpedida'              => IDFacturaExpedida::class,
+                'IDOtro'                         => IDOtro::class,
+                'ImporteSgn14_2'                 => ImporteSgn14_2::class,
+                'Incidencia'                     => Incidencia::class,
+                'ObligadoEmision'                => ObligadoEmision::class,
+                'OperacionExenta'                => OperacionExenta::class,
+                'PersonaFisicaJuridica'          => PersonaFisicaJuridica::class,
+                'PersonaFisicaJuridicaES'        => PersonaFisicaJuridicaES::class,
+                'RechazoPrevio'                  => RechazoPrevio::class,
                 'RegFactuSistemaFacturacion'     => RegFactuSistemaFacturacion::class,
+                'RegistroAlta'                   => RegistroAlta::class,
+                'RegistroAnterior'               => RegistroAnterior::class,
                 'RegistroFactura'                => RegistroFactura::class,
                 'RegistroFacturacionAlta'        => RegistroFacturacionAlta::class,
                 'RegistroFacturacionAnulacion'   => RegistroFacturacionAnulacion::class,
                 'RegistroFacturacionSubsanacion' => Subsanacion::class,
-                // Add additional types here as needed
+                'SistemaInformatico'             => SistemaInformatico::class,
             ],
         ];
 
