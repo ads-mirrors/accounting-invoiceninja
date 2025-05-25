8e30fd730533ae3c91c05ccb2f1c7e61caad84ee
David Bomba <turbo124@gmail.com>
2025-05-25 11:55:52 +1000

    Updated elastic searchable trait

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -12,7 +12,7 @@
 
 namespace App\Models;
 
-use Laravel\Scout\Searchable;
+use Elastic\ScoutDriverPlus\Searchable;
 use App\DataMapper\ClientSync;
 use App\Utils\Traits\AppSetup;
 use App\Utils\Traits\MakesHash;
