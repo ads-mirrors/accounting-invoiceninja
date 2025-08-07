bf5359cb723e68070a605f3c3bb084e7f058516c
David Bomba <turbo124@gmail.com>
2025-08-07 15:16:04 +1000

    Add VerifactuLog

---
+++ app/Models/VerifactuLog.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property int $company_id
+ * @property int $invoice_id
+ * @property string $nif
+ * @property Carbon $date
+ * @property string $invoice_number
+ * @property string $hash
+ * @property string $previous_hash
+ * @property string $status
+ * @property object|null $response
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \App\Models\Company $company
+ * @property-read \App\Models\Invoice $invoice
+ */
+class VerifactuLog extends Model
+{
+    public $timestamps = true;
+    
+    protected $casts = [
+        'date' => 'date',
+        'response' => 'object',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
+}
